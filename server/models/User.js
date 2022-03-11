231e37bdd8bd01fe95792ad50df627ddafdb7711
Artur <xxarturkrossxx@yandex.ru>
2022-03-11 19:24:04 +0300

    server

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const {Schema, model, ObjectId} = require("mongoose")
 const User = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    money: {type: Number, required: true},
 })
 
 module.exports = model('User', User)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  fetch('http://localhost:5000/sayHello')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+
     </div>
   );
 }
