44e9241ad8b84d98ef7e629dad21460beee0be1d
Artur <xxarturkrossxx@yandex.ru>
2022-03-13 09:57:01 +0300

    admin panel

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -5,13 +5,14 @@ import { useDispatch } from 'react-redux';
 
 const Admin = () => {
     const [email, setEmail] = useState("")
+    const [lec, setLec] = useState("")
     const dispatch = useDispatch()
 
     return (
         <div className='beleberda'>
             <p>Добавить пользователю урок</p>
-            <Input value={email} setValue={setEmail} type="text" placeholder="Введите почту пользователя..."/>
-            {/* <Input value={1} setValue={seth1} type="text" placeholder="Введите название урока..."/> */}
+            <input value={email} onChange={(event)=> setEmail(event.target.value)} id='linkText' type="text" className="new-couse-content-inp" placeholder='Почта пользователя' />
+            <input value={lec} onChange={(event)=> setLec(event.target.value)} id='linkText' type="text" className="new-couse-content-inp" placeholder='Название лекции' />
             {/* <button onClick={() => dispatch(newLection(h1, `<p>МНОГО КАКОГО-ТО ТЕКСТА</p>`, 'http://localhost:3000/'))}>ok</button> */}
         </div>
     );
