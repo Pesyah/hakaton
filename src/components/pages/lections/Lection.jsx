b82e2dd7e87347b2090f87c6837d6fa3c1150cca
Tolper4161 <arseniy.z2006@gmail.com>
2022-03-13 09:11:08 +0300

    wegsrdtgfh
    aesrdtfhgj

---
+++ src/components/pages/lections/Lection.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import './Lection.css'
+
+const Lection = () => {
+    return (
+        <section id="lection"></section>
+    );
+};
+export default Lection;
